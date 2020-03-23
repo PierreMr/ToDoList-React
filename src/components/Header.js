414ad55c44f7bd030c753bbd89411164196cefda
PierreMr <mauerpierre@gmail.com>
2020-03-23 18:38:43 +0100

    :snowman:

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,7 @@ import React from "react";
 const Header = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
     <a className="navbar-brand">ToDoApp</a>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNavDropdown"
-      aria-controls="navbarNavDropdown"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <div className="collapse navbar-collapse">
       <ul className="navbar-nav">
         <li className="nav-item active">
           <a className="nav-link">Home</a>
