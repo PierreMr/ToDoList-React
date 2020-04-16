eb688d89e6fe6f482e55b0a6056ac25d6515d425
Pierre MAUER <pierre.mauer@omnilog.fr>
2020-04-16 15:50:35 +0200

    :snowman:

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -3,32 +3,22 @@ import style from "./ToDoList.module.css";
 import AddToDo from "./../containers/AddToDo";
 import RemoveToDoList from "./../containers/RemoveToDoList";
 import ToDo from "./ToDo";
-import { connect } from "react-redux";
 
-const ToDoList = (props) => {
-  const todolist = props.todolist;
-  return (
-    <div className={`card col-3 m-3 ${style.card_custom} shadow-lg`}>
-      <div className="card-body">
-        <h2 className={`card-title ${style.title}`}>{todolist.title}</h2>
-        <AddToDo idToDoList={todolist.id} />
-        <ul className="list-group list-group-flush">
-          {todolist.todos.map((todo) => (
-            <ToDo key={todo.id} {...todo} />
-          ))}
-        </ul>
-      </div>
-      <div className="card-footer">
-        <RemoveToDoList idToDoList={todolist.id} />
-      </div>
+const ToDoList = ({ todolist }) => (
+  <div className={`card col-3 m-3 ${style.card_custom} shadow-lg`}>
+    <div className="card-body">
+      <h2 className={`card-title ${style.title}`}>{todolist.title}</h2>
+      <AddToDo idToDoList={todolist.id} />
+      <ul className="list-group list-group-flush">
+        {todolist.todos.map((todo) => (
+          <ToDo key={todo.id} {...todo} />
+        ))}
+      </ul>
     </div>
-  );
-};
+    <div className="card-footer">
+      <RemoveToDoList idToDoList={todolist.id} />
+    </div>
+  </div>
+);
 
-const mapStateToProps = (state) => {
-  return {
-    todolists: state.todolists,
-  };
-};
-
-export default connect(mapStateToProps)(ToDoList);
+export default ToDoList;
